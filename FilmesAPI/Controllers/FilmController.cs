ef7f0abd246e35200784bdcb3c4854880700104d
Yuri Gonçalves <111998370+Yurig19@users.noreply.github.com>
2023-08-18 14:55:01 -0300

    feat: criando API de filmes

---
+++ FilmesAPI/Controllers/FilmController.cs
@@ -0,0 +1,20 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class FilmController: ControllerBase
+{  
+    private static List<Film> films = new List<Film>();
+
+    [HttpPost]
+    public void AddFilm([FromBody]Film film)
+    {  
+       films.Add(film);
+       Console.WriteLine(film.Title);
+       Console.WriteLine(film.Duration); 
+    }
+        
+}
---
+++ FilmesAPI/Models/Film.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Film
+    {
+        [Required(ErrorMessage = "O título do filme é obrigatório")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "O gênero do filme é obrigatório")]
+        [MaxLength(50, ErrorMessage ="A quantidade de caracteres não pode exceder 50")]
+        public string Gender { get; set; }
+        [Required(ErrorMessage = "A duração do filme é obrigatório")]
+        [Range(70, 600, ErrorMessage ="A duração deve ser entre 70 e 600 minutos")]
+        public int Duration  { get; set; }
+    }
+}
