fca4727d8785e4eab9591959edd85198a4ee1936
Yuri Gonçalves <111998370+Yurig19@users.noreply.github.com>
2023-08-18 17:03:47 -0300

    feat: adicionando Http get e leitura de filmes da API por ID

--- FilmesAPI/Controllers/FilmController.cs
+++ FilmesAPI/Controllers/FilmController.cs
@@ -5,16 +5,29 @@ namespace FilmesAPI.Controllers;
 
 [ApiController]
 [Route("[Controller]")]
-public class FilmController: ControllerBase
-{  
+public class FilmController : ControllerBase
+{
     private static List<Film> films = new List<Film>();
+    private static int id = 0;
 
     [HttpPost]
-    public void AddFilm([FromBody]Film film)
-    {  
-       films.Add(film);
-       Console.WriteLine(film.Title);
-       Console.WriteLine(film.Duration); 
+    public void AddFilm([FromBody] Film film)
+    {
+        film.Id = id++;
+        films.Add(film);
+        Console.WriteLine(film.Title);
+        Console.WriteLine(film.Duration);
     }
-        
+
+    [HttpGet]
+    public IEnumerable<Film> ReadFilms()
+    {
+        return films;
+    }
+
+    [HttpGet("{id}")]
+    public Film? ReadFilmId(int id)
+    {
+        return films.FirstOrDefault(film => film.Id == id);
+    }
 }
